107e16a4180b045f64ae3e63ad31f2776446bda8
MarieLvsq <maarielevesque@gmail.com>
2023-09-12 17:20:05 +0200

    Eureka client Assure OK

--- tpmicroservices_assure/src/main/java/com/urbanisation_si/microservices_assure/MicroservicesAssureApplication.java
+++ tpmicroservices_assure/src/main/java/com/urbanisation_si/microservices_assure/MicroservicesAssureApplication.java
@@ -2,12 +2,14 @@ package com.urbanisation_si.microservices_assure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class MicroservicesAssureApplication {
 
 	public static void main(String[] args) {
